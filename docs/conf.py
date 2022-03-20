da411f71d6c3aeb3114c89700e8eb0d1e061f75c
Chris Holdgraf <choldgraf@berkeley.edu>
2022-03-20 14:09:43 -0700

    issues button

--- docs/conf.py
+++ docs/conf.py
@@ -64,6 +64,7 @@ html_static_path = ["_static"]
 html_theme_options = {
     "repository_url": "https://github.com/executablebooks/meta",
     "use_repository_button": True,
+    "use_issues_button": True,
     "use_edit_page_button": True,
     "path_to_docs": "docs",
 }
